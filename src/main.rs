18fe7bbfcd2d9c7a17b953f8460439b303646305
Li Yichao <liyichao.good@gmail.com>
2015-08-20 01:10:08 +0800

    Test lifetime.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+#![allow(dead_code)]
+use std::ptr;
+
 pub struct LinkedList<T> {
     length: usize,
     list_head: Link<T>,
@@ -5,28 +8,45 @@ pub struct LinkedList<T> {
 }
 
 type Link<T> = Option<Box<Node<T>>>;
-
 struct Rawlink<T> {
     p: *mut T,
 }
 
+impl<T> Copy for Rawlink<T> {}
+impl<T> Clone for Rawlink<T> {
+    #[inline]
+    fn clone(&self) -> Rawlink<T> {
+        Rawlink{p: self.p}
+    }
+}
 struct Node<T> {
     next: Link<T>,
     prev: Rawlink<Node<T>>,
     value: T,
 }
 
-pub struct Iter<'a, T:'a> {
+pub struct Iter<'a, T: 'a> {
     head: &'a Link<T>,
     tail: Rawlink<Node<T>>,
     nelem: usize,
 }
 
-struct a {
-    b: i32
-}
 fn main() {
 
-    let t = &a{b: 2};
-    println!("{}", t.b);
+    let a: i32 = 1;
+    let rawlink = Rawlink{p: ptr::null_mut()};
+    let node = Node{
+        next: None,
+        prev: rawlink,
+        value: 2,
+    };
+    let it: Iter<i32>;
+    {
+        let link = Some(Box::new(node));
+        it = Iter {
+            head: &link,
+            tail: rawlink,
+            nelem: 1,
+        };
+    }
 }
